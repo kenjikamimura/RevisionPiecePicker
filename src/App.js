3b36aabe2dca2801da02058838a48b45dfede942
Kenji Kamimura <kjp.kamimura@gmail.com>
2019-06-04 17:58:18 +1200

    Remembers which pieces were selected after every button click, reset button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import SelectedPieces from "./SelectedPieces";
 import bookOne from "./constants/bookOne";
 class App extends Component {
   state = {
-    selectedPieces: []
+    selectedPieces: [],
+    currentBookOne: bookOne,
+    reset: true
   };
 
   //Todo extract this out to helper file
   compare = (a, b) => {
+    if (a === null || b === null) {
+      return 0;
+    }
     if (a.bookOrder < b.bookOrder) {
       return -1;
     }
@@ -21,43 +26,65 @@ class App extends Component {
     //Todo: Add algoithm which creates an array which will make the probability of
     // getting higher order pieces more likely
 
-    let tempBookOne = bookOne;
+    let tempBookOne = this.state.currentBookOne;
 
     //Todo refactor this to be cleaner, extract out to helper file
-    var firstPiece =
-      tempBookOne[Math.floor(Math.random() * tempBookOne.length)];
+    var firstPiece = tempBookOne.length
+      ? tempBookOne[Math.floor(Math.random() * tempBookOne.length)]
+      : null;
     tempBookOne = tempBookOne.filter(
       song => firstPiece.bookOrder !== song.bookOrder
     );
 
-    var secondPiece =
-      tempBookOne[Math.floor(Math.random() * tempBookOne.length)];
+    var secondPiece = tempBookOne.length
+      ? tempBookOne[Math.floor(Math.random() * tempBookOne.length)]
+      : null;
     tempBookOne = tempBookOne.filter(
       song => secondPiece.bookOrder !== song.bookOrder
     );
-    var thirdPiece =
-      tempBookOne[Math.floor(Math.random() * tempBookOne.length)];
+    var thirdPiece = tempBookOne.length
+      ? tempBookOne[Math.floor(Math.random() * tempBookOne.length)]
+      : null;
+
     tempBookOne = tempBookOne.filter(
       song => thirdPiece.bookOrder !== song.bookOrder
     );
 
-    let selectedPieces = [firstPiece, secondPiece, thirdPiece];
+    let selectedPieces = [firstPiece, secondPiece, thirdPiece].filter(Boolean);
     selectedPieces = selectedPieces.sort(this.compare);
-    this.setState({ selectedPieces });
-  };
 
+    this.setState({
+      selectedPieces,
+      currentBookOne: tempBookOne,
+      reset: false
+    });
+  };
+  resetSongPool = () => {
+    this.setState({ selectedPieces: [], currentBookOne: bookOne, reset: true });
+  };
   render() {
     return (
       <div className="container">
         <h1 className="center blue-text lighten-2">Revision Piece Picker</h1>
-        <SelectedPieces selectedPieces={this.state.selectedPieces} />
-        <div className="center">
-          <a
+        <SelectedPieces
+          selectedPieces={this.state.selectedPieces}
+          reset={this.state.reset}
+        />
+        <div className="row center">
+          <button
             className="waves-effect waves-light btn blue lighten-2 "
             onClick={this.generateRandomPieces}
           >
             Randomize!
-          </a>
+          </button>
+        </div>
+        <div className="row center">
+          <button
+            className="waves-effect waves-light btn grey lighten-2 black-text "
+            onClick={this.resetSongPool}
+          >
+            Reset
+          </button>
         </div>
       </div>
     );
--- src/SelectedPieces.js
+++ src/SelectedPieces.js
@@ -1,16 +1,21 @@
 import React from "react";
 
-const SelectedPieces = ({ selectedPieces }) => {
+const SelectedPieces = ({ selectedPieces, reset }) => {
   const a = selectedPieces.length ? (
     selectedPieces.map(piece => {
-      return (
+      return piece ? (
         <div className="collection-item center" key={piece.bookOrder}>
           <span>{piece.name}</span>
         </div>
-      );
+      ) : null;
     })
-  ) : (
+  ) : reset ? (
     <p className="center">Click Randomize!</p>
+  ) : (
+    <p className="center">
+      Cycled through all possible pieces. <br />
+      Click Reset
+    </p>
   );
   return <div className="collection">{a}</div>;
 };
